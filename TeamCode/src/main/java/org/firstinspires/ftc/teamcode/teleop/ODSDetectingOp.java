5a9c4dd47fd8b7b3846c23f683048965eb1c59d3
ychloelee <18leey94@myahs.org>
2017-03-29 18:39:37 -0700

    added sensor stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ODSDetectingOp.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+
+@TeleOp(name="ODSDetectingOp", group="TeleOp")
+
+public class ODSDetectingOp extends LinearOpMode {
+
+    Drivetrain drivetrain;
+    OpticalDistanceSensor opticalDistanceSensor;
+    ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        drivetrain = new Drivetrain(hardwareMap.dcMotor.get("drive_front_left"), hardwareMap.dcMotor.get("drive_front_right"), hardwareMap.dcMotor.get("drive_back_left"), hardwareMap.dcMotor.get("drive_back_right"));
+        opticalDistanceSensor = hardwareMap.opticalDistanceSensor.get("ods");
+        opticalDistanceSensor.enableLed(true);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            //go over to the wall and read the wall's value, then use this value to do the wall following opmode
+            telemetry.addData("Light Detected: ", opticalDistanceSensor.getLightDetected());
+            telemetry.update();
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TouchSensorOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TouchSensorOp.java
@@ -1,29 +1,36 @@
 package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 
-/*
- *
- * CREATED BY CHLOE, 2/9/17
- */
-
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Drive: MecanumDrive", group="Drive")
-public class MecanumDrive extends OpMode {
-
+public class TouchSensorOp extends OpMode {
     private Drivetrain drivetrain;
+    private TouchSensor touchSensor;
 
     public void init() {
         drivetrain = new Drivetrain(hardwareMap.dcMotor.get("drive_front_left"),hardwareMap.dcMotor.get("drive_front_right"), hardwareMap.dcMotor.get("drive_back_left"),hardwareMap.dcMotor.get("drive_back_right"));
+        touchSensor = hardwareMap.touchSensor.get("sensor_touch");
     }
 
     public void loop() {
-        //mecanum drive algorithm
+        //mechanum drive algorithm
         drivetrain.mechanumDrive(-Math.pow(gamepad1.left_stick_x, 7), -Math.pow(gamepad1.left_stick_y, 7), Math.pow(gamepad1.right_stick_x, 7));
+
+        if(touchSensor.isPressed()) {
+            telemetry.addData("Touch", "Is Pressed");
+        } else {
+            telemetry.addData("Touch", "Is Not Pressed");
+        }
+        telemetry.update();
 
         telemetry.addData("G1_left_stick:", (gamepad1.left_stick_x*100)+ " " + gamepad1.left_stick_y*100);
         telemetry.addData("G1_right_stick:", (gamepad1.right_stick_x*100)+ " " + gamepad1.right_stick_y*100);
+        telemetry.addData("intake", gamepad1.right_trigger);
+        telemetry.addData("right_trigger:", gamepad1.right_bumper);
         telemetry.update();
     }
+
 }
